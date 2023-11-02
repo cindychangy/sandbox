e13a4eae31dc9a618da7d22dd8c53dcd03faa187
Cindy Chang <cindyisachang@gmail.com>
2023-11-02 13:52:32 +0100

    fix formatting

--- src/app/lottie/component/page.js
+++ src/app/lottie/component/page.js
@@ -1,16 +1,17 @@
-'use client'
-import Lottie from "lottie-react";
-import animation from "./animation.json";
+'use client';
+import Lottie from 'lottie-react';
+import animation from './animation.json';
 
 //https://lottiereact.com/ (Documentation)
 
 export default function LottieComponent() {
-
   return (
     <main className="main">
-      <Lottie animationData={animation} loop={true} style={{ width: '600px' }} />
+      <Lottie
+        animationData={animation}
+        loop={true}
+        style={{ width: '600px' }}
+      />
     </main>
-  )
+  );
 }
-
-
