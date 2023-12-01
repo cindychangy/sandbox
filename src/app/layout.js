867cf0a2c8b08955682d502c18cc28d7016bb527
Cindy Chang <cindyisachang@gmail.com>
2023-12-01 15:03:15 +0100

    add pages and fonts

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,27 @@
 import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const mierB = localFont({
+  src: [
+    {
+      path: '../../public/fonts/mierb-bold-webfont.woff2', 
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/mierb-demi-webfont.woff2',
+      weight: '500',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/mierb-bold-webfont.woff2',
+      weight: '700',
+      style: 'normal',
+    }
+  ],
+})
 
 export const metadata = {
   title: 'Code Sandbox',
@@ -11,7 +31,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={mierB.className}>{children}</body>
     </html>
   )
 }
